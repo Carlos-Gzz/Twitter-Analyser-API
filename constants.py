810d711dcbd9af76c833d11a84edd820cefec353
Carlos Gonzalez <carlosmarcelogzz@gmail.com>
2019-11-01 11:42:18 -0600

    first commit

---
+++ constants.py
@@ -0,0 +1,7 @@
+CONSUMER_KEY = 'your consumer key'
+CONSUMER_SECRET = 'your consumer secret key'
+
+REQUEST_TOKEN_URL = 'your request token'
+ACCESS_TOKEN_URL = 'your access token'
+AUTHORIZATION_URL = 'your authorization token'
+#These keys/tokens has been taken out due to the protection of my own personal information.
